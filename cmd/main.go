cd540303092c4a06802893fae9a6857558141d78
Tom5521 <thomas.bored5521@gmail.com>
2024-06-10 00:14:43 -0400

    fix: Add minimum args to root command.

--- cmd/main.go
+++ cmd/main.go
@@ -12,6 +12,7 @@ var (
 		Use:   "gonotes",
 		Short: "A note manager for the terminal",
 		Long:  "A CLI that allows you to manipulate and manage notes from your terminal using your favorite editor.",
+		Args:  cobra.MinimumNArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) (err error) {
 			err = cmd.ParseFlags(args)
 			if err != nil {
