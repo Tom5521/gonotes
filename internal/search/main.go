d73f41f376528bac706761f381f49528f1bd1c7d
Tom5521 <thomas.bored5521@gmail.com>
2024-06-13 06:25:52 -0400

    style(internal/search): Improve organization
    Move deep function into deep.go file

--- internal/search/main.go
+++ internal/search/main.go
@@ -2,10 +2,8 @@ package search
 
 import (
 	"errors"
-	"strconv"
 
 	"github.com/Tom5521/gonotes/internal/db"
-	"github.com/Tom5521/gonotes/internal/options"
 )
 
 var ErrNotFound = errors.New("File not found")
@@ -26,46 +24,4 @@ func ByName(name string) (db.File, error) {
 		}
 	}
 	return db.File{}, ErrNotFound
-}
-
-func Deep(arg string) (file db.File, err error) {
-	switch {
-	case options.ByID:
-		var id int
-		id, err = strconv.Atoi(arg)
-		if err != nil {
-			return
-		}
-		file, err = ByID(uint(id))
-	case options.ByName:
-		file, err = ByName(arg)
-	case options.CustomID >= 0 || options.CustomName != "":
-		file, err = ByID(uint(options.CustomID))
-		if err != nil {
-			file, err = ByName(options.CustomName)
-			if err != nil {
-				file, err = ByName(arg)
-			}
-		}
-	default:
-		file, err = ByName(arg)
-		if err != nil {
-			var id int
-			id, err = strconv.Atoi(arg)
-			if err != nil {
-				return file, ErrNotFound
-			}
-			file, err = ByID(uint(id))
-		}
-
-	}
-	if (options.CustomID != -1 || options.CustomName != "") &&
-		(file.ID != uint(options.CustomID) || file.Name != options.CustomName) {
-		err = ErrNotFound
-	}
-	if file.Temporal && !options.Temporal {
-		err = ErrNotFound
-	}
-
-	return
 }
