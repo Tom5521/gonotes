b8c26fc2f94223b318a3f82d4a13ef4fd046a068
Tom5521 <thomas.bored5521@gmail.com>
2024-06-14 11:45:51 -0400

    feat: Add --version command

--- cmd/main.go
+++ cmd/main.go
@@ -5,15 +5,17 @@ import (
 	"os"
 
 	"github.com/Tom5521/gonotes/internal/db"
+	"github.com/Tom5521/gonotes/meta"
 	"github.com/spf13/cobra"
 )
 
 var (
 	root = cobra.Command{
-		Use:   AppName,
-		Short: "A note manager for the terminal",
-		Long:  "A CLI that allows you to manipulate and manage notes from your terminal using your favorite editor.",
-		Args:  cobra.MinimumNArgs(1),
+		Use:     AppName,
+		Short:   "A note manager for the terminal",
+		Long:    "A CLI that allows you to manipulate and manage notes from your terminal using your favorite editor.",
+		Version: meta.Version,
+		Args:    cobra.MinimumNArgs(1),
 		PersistentPreRunE: func(cmd *cobra.Command, args []string) (err error) {
 			err = db.LoadFiles()
 			if err != nil {
