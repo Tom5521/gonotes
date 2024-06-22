72c53d7e358d18290fe5b48b6cda26fbe8e54605
Tom5521 <thomas.bored5521@gmail.com>
2024-06-22 16:55:29 -0400

    fix: Fix searching algorithm

--- internal/search/deep.go
+++ internal/search/deep.go
@@ -4,8 +4,7 @@
 package search
 
 import (
-	"errors"
-	"strconv"
+	"fmt"
 
 	"github.com/Tom5521/gonotes/internal/db"
 	"github.com/Tom5521/gonotes/internal/options"
@@ -23,8 +22,73 @@ func Deep(arg string) (file db.File, err error) {
 	default:
 		files = db.Files
 	}
+	compare := func(file db.File) (db.File, error) {
+		if options.Filetype != file.Type {
+			err = fmt.Errorf(
+				"types do not match, type requested: %s | type found: %s",
+				options.Filetype,
+				file.Type,
+			)
+		}
+		if options.CustomName != "" && options.CustomName != file.Name {
+			err = fmt.Errorf(
+				"names do not match, type requested: %s | type found: %s",
+				options.CustomName,
+				file.Name,
+			)
+		}
+		if options.CustomID != -1 && options.CustomID != int(file.ID) {
+			err = fmt.Errorf(
+				"IDs do not match, type requested: %v | type found: %v",
+				options.CustomID,
+				file.ID,
+			)
+		}
 
-	unfilteredFiles := files
+		if err != nil {
+			return db.File{}, err
+		}
+		return file, nil
+	}
+
+	// Try to get file by the name.
+	fileByName, errByName := byName(files, arg)
+
+	// Try to get file by the ID
+	fileByID, errByID := byID(files, arg)
+
+	foundByName := errByName == nil
+	foundByID := errByID == nil
+
+	switch {
+	case options.ByName && foundByName:
+		return compare(fileByName)
+	case options.ByID && foundByID:
+		return compare(fileByID)
+	case !foundByName && foundByID:
+		return compare(fileByID)
+	case !foundByID && foundByName:
+		return compare(fileByName)
+	case !foundByName && !foundByID:
+		return db.File{}, ErrNotFound
+	}
+
+	return
+}
+
+/*
+func Deep(arg string) (file db.File, err error) {
+	var files []db.File
+
+	// Filter 1: File location.
+	switch {
+	case options.Temporal:
+		files = db.TemporalFiles()
+	case options.Normal:
+		files = db.NormalFiles()
+	default:
+		files = db.Files
+	}
 
 	var found bool
 
@@ -40,26 +104,30 @@ func Deep(arg string) (file db.File, err error) {
 			name = options.CustomName
 		}
 		files, found = filterByName(name, files)
-		if !found {
-			files = unfilteredFiles
+		if len(files) > 1 {
+			return file, errors.New("more than one element with the same name was found")
 		}
 	}
 
 	// Filter 3: ID.
-	if !found || !argIsName {
+	if !argIsName {
 		var id = uint64(options.CustomID)
-		if !idIsProvided {
+		if idIsProvided {
 			id, err = atoui(arg)
 			if err != nil {
 				return
 			}
 		}
 		files, found = filterByID(id, files)
+		if !found {
+			return file, errors.New("no id found")
+		}
 	}
-	if !found {
+	if len(files) == 0 {
 		return file, errors.New("file name/id not found")
 	}
-	if len(files) == 1 {
+
+	if found {
 		return files[0], nil
 	}
 
@@ -108,21 +176,9 @@ func filterByType(t string, files []db.File) (filteredFiles []db.File, found boo
 func atoui(s string) (uint64, error) {
 	return strconv.ParseUint(s, 10, strconv.IntSize)
 }
+*/
 
 /*
-func Deep(arg string) (file db.File, err error) {
-	var files []db.File
-
-	// Filter 1: File location.
-	switch {
-	case options.Temporal:
-		files = db.TemporalFiles()
-	case options.Normal:
-		files = db.NormalFiles()
-	default:
-		files = db.Files
-	}
-
 	var argIsName bool
 	if !options.ByID || options.CustomID != -1 || options.ByName {
 		argIsName = true
@@ -193,60 +249,4 @@ func Deep(arg string) (file db.File, err error) {
 	}
 
 	file = files[0]
-
-	/*
-		compare := func(file db.File) (db.File, error) {
-			if options.Filetype != file.Type {
-				err = fmt.Errorf(
-					"types do not match, type requested: %s | type found: %s",
-					options.Filetype,
-					file.Type,
-				)
-			}
-			if options.CustomName != "" && options.CustomName != file.Name {
-				err = fmt.Errorf(
-					"names do not match, type requested: %s | type found: %s",
-					options.CustomName,
-					file.Name,
-				)
-			}
-			if options.CustomID != -1 && options.CustomID != int(file.ID) {
-				err = fmt.Errorf(
-					"IDs do not match, type requested: %v | type found: %v",
-					options.CustomID,
-					file.ID,
-				)
-			}
-
-			if err != nil {
-				return db.File{}, err
-			}
-			return file, nil
-		}
-
-		// Try to get file by the name.
-		fileByName, errByName := byName(files, arg)
-
-		// Try to get file by the ID
-		fileByID, errByID := byID(files, arg)
-
-		foundByName := errByName == nil
-		foundByID := errByID == nil
-
-		switch {
-		case options.ByName && foundByName:
-			return compare(fileByName)
-		case options.ByID && foundByID:
-			return compare(fileByID)
-		case !foundByName && foundByID:
-			return compare(fileByID)
-		case !foundByID && foundByName:
-			return compare(fileByName)
-		case !foundByName && !foundByID:
-			return db.File{}, ErrNotFound
-		}
-	* /
-	return
-}
-
 */
